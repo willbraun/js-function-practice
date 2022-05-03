72b841984d812688ec0a7e7a8352145603236966
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-03 15:00:15 -0400

    created an IIFE and moved a global variable declaration and a ssignment inside of it so it cannot be changed in the console

--- refactor.js
+++ refactor.js
@@ -229,7 +229,17 @@ var decreaseScore = function() {
 };
 
 // Put your answer below -------------------------
+(function() {
+	var score = 0;
+})();
 
+var increaseScore = function() {
+  score++;
+};
+
+var decreaseScore = function() {
+  score--;
+};
 
 
 // -----------------------------------------------
